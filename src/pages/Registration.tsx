7117d0d9debad5be5eb66fed5f6d670df1787488
Sofia <sofiacelestrin@gmail.com>
2024-11-18 10:15:18 -0500

    Design of Registration Page

--- src/pages/Registration.tsx
+++ src/pages/Registration.tsx
@@ -17,19 +17,15 @@ function Registration() {
     setErrorMessage("");
     setSuccessMessage("");
 
-    // Check if passwords match
     if (password !== confirmPassword) {
       setErrorMessage("Passwords do not match.");
       return;
     }
 
-    // Create new user in auth.users
-    const { data: signUpData, error: signUpError } = await supabase.auth.signUp(
-      {
-        email,
-        password,
-      },
-    );
+    const { data: signUpData, error: signUpError } = await supabase.auth.signUp({
+      email,
+      password,
+    });
 
     if (signUpError) {
       console.error("Error signing up:", signUpError.message);
@@ -37,87 +33,169 @@ function Registration() {
       return;
     }
 
-    // If account creation is successful, then create an entry in the public.users table
-    //By default, the user object should be truthy
     const { user } = signUpData;
-
     const { data: userData, error: userInsertError } = await supabase
       .from("users")
-      .insert([
-        {
-          id: user?.id,
-          full_name: fullName,
-        },
-      ]);
+      .insert([{ id: user?.id, full_name: fullName }]);
 
     if (userInsertError) {
       console.error("Error inserting user data:", userInsertError.message);
       setErrorMessage("Account created, but additional data failed to save.");
     } else {
-      console.log(
-        "User registered and additional data saved successfully:",
-        userData,
-      );
       setSuccessMessage("Account created!");
     }
   }
 
   useEffect(() => {
-    if (successMessage === "") return;
-    const id = setInterval(() => {
-      setCountdown((count) => count - 1);
+    if (!successMessage) return;
 
-      if (countdown - 1 <= 0) {
-        navigate("/create-character");
-      }
+    const interval = setInterval(() => {
+      setCountdown((count) => {
+        if (count <= 1) {
+          clearInterval(interval);
+          navigate("/create-character");
+          return count;
+        }
+        return count - 1;
+      });
     }, 1000);
 
-    return () => clearInterval(id);
-  }, [successMessage, countdown, navigate]);
+    return () => clearInterval(interval);
+  }, [successMessage, navigate]);
 
   return (
-    <div>
-      <h1>Registration Page</h1>
-      <form id="login-form" onSubmit={signUpNewUser}>
-        <input
-          type="email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          placeholder="Email"
-          required
-        />
-        <input
-          type="text"
-          value={fullName}
-          onChange={(e) => setFullName(e.target.value)}
-          placeholder="Full Name"
-          required
-        />
-        <input
-          type="password"
-          placeholder="Password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          required
-        />
-        <input
-          type="password"
-          placeholder="Confirm Password"
-          value={confirmPassword}
-          onChange={(e) => setConfirmPassword(e.target.value)}
-          required
-        />
-        <button type="submit">Create Account</button>
-        {errorMessage && <p style={{ color: "red" }}>{errorMessage}</p>}
-        {successMessage && (
-          <p style={{ color: "green" }}>
-            {successMessage}. Directing you to character creation in {countdown}{" "}
-            seconds
-          </p>
-        )}
-      </form>
-    </div>
+    <>
+
+<header className="bg-blue-600 p-4 text-white">
+  <div className="flex items-center">
+
+    <img
+      src="/src/1logo.svg"
+      alt="Company Logo"
+      className="h-12 w-auto mr-4"
+    />
+    <div className="text-4xl font-bold">Balance Buddy</div>
+  </div>
+</header>
+
+
+
+      <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+        <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+          <img
+            src="/src/1logo.svg"
+            alt="Logo"
+            className="mx-auto h-64 w-auto"
+          />
+          <h2 className="mt-10 text-center text-3xl font-bold tracking-tight text-gray-900">
+            Create Your Account
+          </h2>
+        </div>
+
+        <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+          <form onSubmit={signUpNewUser} className="space-y-6">
+            <div>
+              <label
+                htmlFor="email"
+                className="block text-sm font-medium text-gray-900"
+              >
+                Email Address
+              </label>
+              <div className="mt-2">
+                <input
+                  id="email"
+                  name="email"
+                  type="email"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  required
+                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm"
+                />
+              </div>
+            </div>
+
+            <div>
+              <label
+                htmlFor="fullName"
+                className="block text-sm font-medium text-gray-900"
+              >
+                Full Name
+              </label>
+              <div className="mt-2">
+                <input
+                  id="fullName"
+                  name="fullName"
+                  type="text"
+                  value={fullName}
+                  onChange={(e) => setFullName(e.target.value)}
+                  required
+                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm"
+                />
+              </div>
+            </div>
+
+            <div>
+              <label
+                htmlFor="password"
+                className="block text-sm font-medium text-gray-900"
+              >
+                Password
+              </label>
+              <div className="mt-2">
+                <input
+                  id="password"
+                  name="password"
+                  type="password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                  required
+                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm"
+                />
+              </div>
+            </div>
+
+            <div>
+              <label
+                htmlFor="confirmPassword"
+                className="block text-sm font-medium text-gray-900"
+              >
+                Confirm Password
+              </label>
+              <div className="mt-2">
+                <input
+                  id="confirmPassword"
+                  name="confirmPassword"
+                  type="password"
+                  value={confirmPassword}
+                  onChange={(e) => setConfirmPassword(e.target.value)}
+                  required
+                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm"
+                />
+              </div>
+            </div>
+
+            <button
+              type="submit"
+              className="flex w-full justify-center rounded-md bg-blue-600 px-3 py-1.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+            >
+              Create Account
+            </button>
+
+            {errorMessage && (
+              <p className="mt-2 text-sm text-red-600">{errorMessage}</p>
+            )}
+            {successMessage && (
+              <p className="mt-2 text-sm text-green-600">
+                {successMessage}. Redirecting in {countdown} seconds...
+              </p>
+            )}
+          </form>
+        </div>
+      </div>
+    </>
   );
 }
 
 export default Registration;
+
+
